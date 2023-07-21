45c1f3ef05ca8964ee0ab1c8f0c6fe9db5ccdfef
mpondev <mario@mpondev.com>
2023-07-21 12:57:41 +0200

    Navbar design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,31 @@
 import './Navbar.scss';
 
 function Navbar() {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="logo" />
+        <span>mponadmin</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="search icon" className="icon" />
+        <img src="/app.svg" alt="app icon" className="icon" />
+        <img src="/expand.svg" alt="expand icon" className="icon" />
+        <div className="notifications">
+          <img src="/notifications.svg" alt="notification logo" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+            alt="user image"
+          />
+          <span>Jane</span>
+        </div>
+        <img src="/settings.svg" alt="settings icon" className="icon" />
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
